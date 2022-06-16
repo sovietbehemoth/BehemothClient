16513fb831d97f92596010686dd9adfe67e5e714
sovietbehemoth <djbandini@gmail.com>
2022-06-15 21:11:25 -0400

    Fixed a bug that excluded some commands from usage + a few other bugs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 
 
 import BehemothClientSocket from './socket/socket.js';
-import { KeyHandler } from './keys/KeyHandler.js';
+import { KeyHandler, term } from './keys/KeyHandler.js';
 
 import API_chn from "./commands/chn.js";
 import API_srv from './commands/srv.js';
 import API_exit from './commands/exit.js';
+import API_q from "./commands/_q.js";
+import API_status from "./commands/status.js";
+import API_me from "./commands/me.js";
 
 import { exec } from 'child_process';
 import { stdin } from 'process';
@@ -66,33 +69,21 @@ class BehemothClient {
         Channels: [],                   //List of channels in the currently indexed server.
         SelectedServer: undefined,      //Server to list channels from.
         SelectedChannel: undefined,     //Channel to receive messages from.
-        Mode: "CMD"                     //Mode determines client behavior. CMD mode is without message sending.
+        Mode: "CMD",                    //Mode determines client behavior. CMD mode is without message sending.
     };
 
-
-    buildMenuObject() {
-        const terminalWidth = process.stdout.rows;
-
-        for (let i = 0; i < terminalWidth; i++) {
-            this.MenuObject += "_";
-        } this.MenuObject += "\n\n";
-
-        for (let i = 0; i < terminalWidth; i++) {
-            this.MenuObject += "_";
-        } this.MenuObject += "\n";
-
-    }
+    
 
     //Creates a notification sound based on the sound file listed in the settings file.
     ping() {
-        exec(`python ../internals/sound.py ${this.Settings.NotificationSound}`);
+        exec(`python ./internals/sound.py ${this.Settings.NotificationSound}`);
     }
 
 
-
-
-
-
+   
+   
+   
+   
 
 
 
@@ -103,7 +94,7 @@ class BehemothClient {
     async appendInformation() {
 
         //Ensure that file exists.
-        fs.access("./data/settings.json", fs.constants.R_OK, (err) => {
+        fs.access("./src/data/settings.json", fs.constants.R_OK, (err) => {
             if (err) {
                 this.Keys.displayMessage("Error: Essential 'settings.json' file not found.\n");
                 process.exit(1);
@@ -111,7 +102,7 @@ class BehemothClient {
         });
         
         //Read file.
-        const buffer = await XreadFile("./data/settings.json");
+        const buffer = await XreadFile("./src/data/settings.json");
 
         try {
             this.Settings = await JSON.parse(buffer);
@@ -134,7 +125,7 @@ class BehemothClient {
      * @param {*} callback (ctx, args) The callback to the command. ctx: Context to the BehemothClient class, args: Arguments to the command, split on whitespace.
      * @param {*} options Optional extra options.
     */
-    newCommand(name, callback, options = {default_args: false}) {
+    newCommand(name, callback, options = {default_args: false, mode: "ANY"}) {
 
         //No duplicate identifiers allowed in API.
         for (let i = 0 ; i < this.API.length; i++) {
@@ -156,28 +147,42 @@ class BehemothClient {
      * @param {*} name Name of command (string).
      * @param {*} args Arguments supplied to command (string[]).
      */
-    async invokeCommand(name, args) {
+    async invokeCommand(name, args, nothrow = false) {
         for (let i = 0; i < this.API.length; i++) {
             if (this.API[i].name === name.trim()) {
+
+                if (this.API[i].options.mode === "CMD" && this.Meta.Mode !== "CMD") {
+                    this.Keys.displayMessage(`~BehemothClient: Command '${this.API[i].name}' is only usable at top level.`);
+                    return;
+                } else if (this.API[i].options.mode === "MSG" && this.Meta.Mode !== "MSG") {
+                    this.Keys.displayMessage(`~BehemothClient: Command '${this.API[i].name}' is only usable in message mode.`);
+                }
+
                 await this.API[i].call(this, args);
                 return;
             }
         }
-        throw new Error(`Command by the name '${name}' does not exist.`);
+        if (!nothrow) throw new Error(`Command by the name '${name}' does not exist.`);
+
+        this.Keys.displayMessage(`~BehemothClient: Could not find command by the name of '${name}'.`);
     }
 
     /**Appends all built-in commands to the command list. DO NOT INVOKE. */
     builtinCommandsPush() {
-        this.newCommand("chn", API_chn);
-        this.newCommand("srv", API_srv);
+        this.newCommand("chn", API_chn, {mode: "CMD"});
+        this.newCommand("srv", API_srv, {mode: "CMD"});
         this.newCommand("exit", API_exit, {default_args: true});
+        this.newCommand("q", API_q, {default_args: true});
+        this.newCommand("status", API_status, {default_args: true});
+        this.newCommand("refresh", () => this.Socket.updateUserInfo(), {default_args:true});
+        this.newCommand("me", API_me, {default_args: true});
     }
 
-
+    //Deliberately crashes the client, logging @message.
     createCrash(message) {
         console.log("// Uh oh, it looks like BehemothClient got mad and crashed.");
         console.log(message);
-        console.log(`  PID: ${process.pid}\n  Platform: ${process.platform}\n  CPU Time: ${process.resourceUsage().systemCPUTime}\n  CPU: ${process.cpuUsage().system}\n  Heap Size: ${process.memoryUsage().heapUsed} / ${process.memoryUsage().heapTotal}\n  HRTime: ${process.hrtime()[0]} & ${process.hrtime()[1]}`);
+        console.log(`  PID: ${process.pid}\n  Platform: ${process.platform}`);
         process.exit(1);
     }
 
@@ -208,18 +213,18 @@ class BehemothClient {
 
 
     constructor() {
+
         this.Keys = new KeyHandler(this);
         this.builtinCommandsPush();
         this.Keys.displayMessage("Starting BehemothClient...\n");
         this.handleErrors();
 
-        //this.createCrash(11);
-        console.log("dd");
 
         this.appendInformation().then(async () => {
             this.Socket = new BehemothClientSocket();
             await this.Socket.startSocketPub(this);
             await this.invokeCommand("srv", ["-ls"]);
+            process.stdout.write(this.MenuObject + '\n');
         });
 
         
@@ -227,7 +232,3 @@ class BehemothClient {
 }
 
 const Client = new BehemothClient();
-
-//const encode = new TextEncoder();
-//process.stdin.write(encode.encode("example: "));
-
